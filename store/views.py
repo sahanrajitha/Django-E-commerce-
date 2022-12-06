b416ca019329314c406cf7d0b39372ad0b6a9ba2
Sahan rajitha <84848431+sahanrajitha@users.noreply.github.com>
2022-12-06 10:33:40 +0530

    Add files via upload

---
+++ store/views.py
@@ -0,0 +1,93 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import *
+import json
+from .utils import *
+import datetime
+
+def about(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+
+    context = {'products': Product.objects.all(), 'cartItems':cartItems}
+    return render(request, 'store/about.html', context)
+
+
+def store(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+
+    context = {'products': Product.objects.all(), 'cartItems':cartItems}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+
+    context = {'items':items, 'order':order, 'cartItems':cartItems, 'shipping': False}
+    return render(request, 'store/cart.html', context)
+
+
+
+def checkout(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+
+    context = {'items':items, 'order':order, 'cartItems':cartItems, 'shipping': False}
+    return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
+
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    else:
+        customer, order = guestOrder(data, request)
+
+    total = float(data['form']['total'])
+    order.transfaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer = customer,
+            order = order,
+            address = data['shipping']['address'],
+            city = data['shipping']['city'],
+            state = data['shipping']['state'],
+            zipcode = data['shipping']['zipcode'],
+        )
+    return JsonResponse('Payment complete', safe=False)
